cb02d09f0a83401c5688ab268d6dfb827eb474a8
Matthew Gonzales <mattgonzales3@yahoo.com>
2022-06-19 23:31:17 -0500

    updated tag-routes

--- models/Tag.js
+++ models/Tag.js
@@ -6,6 +6,12 @@ class Tag extends Model {}
 
 Tag.init(
   {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
     tag_name: {
       type: DataTypes.STRING,
       allowNull: false

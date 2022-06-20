a9c90130fc38de70e070238c181cb295cdd44333
Matthew Gonzales <mattgonzales3@yahoo.com>
2022-06-19 20:22:53 -0500

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,25 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    category_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+      unique: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL(10,2),
+      allowNull: false
+    },
+    stock: {
+      type: DataTypes.DECIMAL(10,2),
+      allowNull: false
+    },
   },
   {
     sequelize,
